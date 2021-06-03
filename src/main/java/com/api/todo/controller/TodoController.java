ff471356b552761b1284eb2b97023fd62b3ee8b7
alan santos <alanjosedossantos011@gmail.com>
2021-06-03 10:35:21 -0300

    created model, service, repository e controller todo

---
+++ src/main/java/com/api/todo/controller/TodoController.java
@@ -0,0 +1,4 @@
+package com.api.todo.controller;
+
+public class TodoController {
+}
---
+++ src/main/java/com/api/todo/service/TodoService.java
@@ -0,0 +1,7 @@
+package com.api.todo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoService {
+}
