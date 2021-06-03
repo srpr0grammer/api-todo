f051d2c7dd987a9c7683fe868c5aa1bee0ba49e8
alan santos <alanjosedossantos011@gmail.com>
2021-06-03 10:38:31 -0300

    finalizando model

--- src/main/java/com/api/todo/model/Todo.java
+++ src/main/java/com/api/todo/model/Todo.java
@@ -4,12 +4,21 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
 import java.time.LocalDate;
 
+@Entity(name = "tb_todo")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Todo {
+public class Todo implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String descricao;
     private LocalDate dataParaFinalizar;
